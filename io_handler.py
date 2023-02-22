3aa06e84ff6f0d4850f9ea15669b0ffed58252c5
Rafael Ottersberg <rafael.ottersberg@unibe.ch>
2023-02-22 23:58:50 +0100

    initail commit

--- io_handler.py
+++ io_handler.py
@@ -12,11 +12,20 @@ def read_data(filename="solar_jfc.dat"):
         au = 1.5e11
         m_sol = 2e30
         day = 24.0 * 60.0 * 60.0
+        masses = []
+        pos_x = []
+        pos_y = []
+        pos_z = []
+        vel_x = []
+        vel_y = []
+        vel_z = []
         for i, row in enumerate(reader):
             if i > 0:
-                pos = np.array(row[2:5], dtype=np.float64) * au
-                vel = np.array(row[5:8], dtype=np.float64) * au / day
-                acc = np.zeros(3)
-                b = Body(float(row[1]) * m_sol, pos, vel, acc)
-                bodies.append(b)
-    return bodies
+                pos_x.append(float(row[2]) * au)
+                pos_y.append(float(row[3]) * au)
+                pos_z.append(float(row[4]) * au)
+                vel_x.append(float(row[5]) * au / day)
+                vel_y.append(float(row[6]) * au / day)
+                vel_z.append(float(row[7]) * au / day)
+                masses.append(float(row[1]) * m_sol)
+    return masses, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z
--- main.py
+++ main.py
@@ -2,38 +2,60 @@ import timeit
 import numpy as np
 
 from io_handler import read_data
+from numba import jit
+
+@jit(nopython=True)
+def calc_direct_forces(pos_x, pos_y, pos_z, masses, g):
+    a_x = []
+    a_y = []
+    a_z = []
+    for i in range(len(masses)):
+        a_x_i = 0
+        a_y_i = 0
+        a_z_i = 0
+        for j in range(len(masses)):
+            r_x = pos_x[j] - pos_x[i]
+            r_y = pos_y[j] - pos_y[i]
+            r_z = pos_z[j] - pos_z[i]
+            r = np.sqrt(r_x ** 2 + r_y ** 2 + r_z ** 2)
+            if r != 0:
+                a_x_i += g * masses[j] * r_x / (r ** 3.0)
+                a_y_i += g * masses[j] * r_y / (r ** 3.0)
+                a_z_i += g * masses[j] * r_z / (r ** 3.0)
+        a_x.append(a_x_i)
+        a_y.append(a_y_i)
+        a_z.append(a_z_i)
+
+    return a_x, a_y, a_z
+@jit(nopython=True)
+def run(masses, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, t, dt, g):
+    for _ in range(t):
+        # LEAPFROG
+        for i in range(len(masses)):
+            pos_x[i] = pos_x[i] + vel_x[i] * 0.5 * dt
+            pos_y[i] = pos_y[i] + vel_y[i] * 0.5 * dt
+            pos_z[i] = pos_z[i] + vel_z[i] * 0.5 * dt
+
+        a_x, a_y, a_z = calc_direct_forces(pos_x, pos_y, pos_z, masses, g)
+        
+        for i in range(len(masses)):
+            vel_x[i] = vel_x[i] + a_x[i] * 0.5 * dt
+            vel_y[i] = vel_y[i] + a_y[i] * 0.5 * dt
+            vel_z[i] = vel_z[i] + a_z[i] * 0.5 * dt
+
+            pos_x[i] = pos_x[i] + vel_x[i] * 0.5 * dt
+            pos_y[i] = pos_y[i] + vel_y[i] * 0.5 * dt
+            pos_z[i] = pos_z[i] + vel_z[i] * 0.5 * dt
 
 
-class Simulation:
-
-    def __init__(self, bodies, t=1000, dt=1.0, g=6.67408e-11):
-        self.bodies = bodies
-        self.t = t
-        self.dt = dt
-        self.g = g
-
-    def calc_direct_forces(self):
-        for b_i in self.bodies:
-            b_i.acc = np.zeros(3)
-            for b_j in self.bodies:
-                r_vec = b_j.pos - b_i.pos
-                r = np.linalg.norm(r_vec)
-                if r != 0:
-                    b_i.acc += self.g * b_j.m * r_vec / (r ** 3.0)
-
-    def run(self):
-        for ti in range(self.t):
-            # LEAPFROG
-            for b in self.bodies:
-                b.pos = b.pos + b.vel * 0.5 * self.dt
-            self.calc_direct_forces()
-            for b in self.bodies:
-                b.vel = b.vel + b.acc * self.dt
-                b.pos = b.pos + b.vel * 0.5 * self.dt
 
 
 if __name__ == '__main__':
-    bodies = read_data()
-    Sim = Simulation(bodies, t=100, dt=24 * 60 * 60)
-    result = np.array(timeit.repeat(Sim.run, number=1, repeat=10))
-    print(f"This took {result.mean():.2f} +/- {result.std():.2f} seconds")
+    masses, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z = read_data()
+    t=100
+    dt=24 * 60 * 60
+    g=6.67408e-11
+    test = lambda: run(masses, pos_x, pos_y, pos_z, vel_x, vel_y, vel_z, t, dt, g)
+    test()
+    result = np.array(timeit.repeat(test, number=1, repeat=10))
+    print(f"This took {result.mean():.3f} +/- {result.std():.3f} seconds")
